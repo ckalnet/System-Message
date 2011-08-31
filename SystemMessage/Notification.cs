84f784419f3625fbcf08fa8ec677eb222d50906a
unknown <ckal@.(none)>
2011-08-31 16:30:48 -0400

    First Commit

---
+++ SystemMessage/Notification.cs
@@ -0,0 +1,17 @@
+﻿using SignalR.Hubs;
+
+namespace SystemMessage
+{
+    public class Notification : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.addMessage(message);
+        }
+
+        public void ApplicationMaintenance()
+        {
+            Clients.alertApplicationMaintenance();
+        }
+    }
+}
